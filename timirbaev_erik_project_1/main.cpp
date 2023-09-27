37558d52e08eb0465de9967915d58151369aa6ff
eriktimirbaev <timirbaeverik@yandex.ru>
2023-09-27 22:23:26 +0300

    Added a separate function for the menu and added processing of the entered data

--- timirbaev_erik_project_1/main.cpp
+++ timirbaev_erik_project_1/main.cpp
@@ -2,46 +2,62 @@
 
 using namespace std;
 
+void menu() {
+	while (true)
+	{
+		cout << "1. Add a pipe" << endl
+			<< "2. Add a compressor station" << endl
+			<< "3. Viewing all objects" << endl
+			<< "4. Edit a pipe" << endl
+			<< "5. Edit a compressor station" << endl
+			<< "6. Save all" << endl
+			<< "7. Download" << endl
+			<< "0. Exit" << endl;
+		int num;
+		cin >> num;
+		if (!cin)
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Error! Enter an integer from 0 to 7." << endl;
+		}
+		else
+		{
+			switch (num)
+			{
+			case 1:
+				cout << "1";
+				break;
+			case 2:
+				cout << "2";
+				break;
+			case 3:
+				cout << "3";
+				break;
+			case 4:
+				cout << "4";
+				break;
+			case 5:
+				cout << "5";
+				break;
+			case 6:
+				cout << "6";
+				break;
+			case 7:
+				cout << "7";
+				break;
+			case 0:
+				cout << "0";
+				break;
+			default:
+				cout << "Error! Enter an integer from 0 to 7." << endl;
+			}
+		}
+	}
+}
+
 int main() {
 	setlocale(LC_ALL, "RU");
-	cout << "1. Добавить трубу" << endl
-		 <<	"2. Добавить КС" << endl
-		 <<	"3. Просмотр всех объектов" << endl
-		 <<	"4. Редактировать трубу" << endl
-		 <<	"5. Редактировать КС" << endl
-		 <<	"6. Сохранить" << endl
-		 <<	"7. Загрузить" << endl
-		 <<	"0. Выход" << endl;
-	int menu;
-	cin >> menu;
-	switch (menu)
-	{
-	case 1:
-		cout << "1";
-		break;
-	case 2:
-		cout << "2";
-		break;
-	case 3:
-		cout << "3";
-		break;
-	case 4:
-		cout << "4";
-		break;
-	case 5:
-		cout << "5";
-		break;
-	case 6:
-		cout << "6";
-		break;
-	case 7:
-		cout << "7";
-		break;
-	case 0:
-		cout << "0";
-		break;
-	default:
-		cout << "Ошибка!" << endl;
-	}
+	menu();
 	return 0;
 }
