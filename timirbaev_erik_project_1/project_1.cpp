d092afad0f570bffe8ccac38b7ef47faf165cc55
eriktimirbaev <timirbaeverik@yandex.ru>
2023-10-15 20:22:09 +0300

    Fixed the bugs

--- timirbaev_erik_project_1/project_1.cpp
+++ timirbaev_erik_project_1/project_1.cpp
@@ -4,199 +4,157 @@
 
 using namespace std;
 
-struct Pipe
-{
+struct Pipe {
 	string name;
 	int length;
 	int diameter;
 	bool repair;
 };
 
-struct Station
-{
+struct Station {
 	string name;
 	int workshops;
 	int workshops_in_operation;
-	double effectiveness;
+	int effectiveness;
 };
 
-void bufer() {
-	cin.clear();
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-}
-
-int get_length() {
-	while (true)
-	{
-		bufer();
-		int len;
-		cin >> len;
-		if (!cin)
-		{
-			cout << "Enter the length of the pipe: ";
-		}
-		else
-		{
-			return len;
-		}
+bool int_check(string input) {
+	for (int i = 0; i < input.length(); i++) {
+		if (isdigit(input[i])) continue;
+		else return false;
 	}
+	if (input.length() == 0) return false;
+	return true;
 }
 
-int get_diameter() {
-	while (true)
-	{
-		bufer();
-		int diam;
-		cin >> diam;
-		if (!cin)
-		{
-			cout << "Enter the diameter of the pipe: ";
-		}
-		else
-		{
-			return diam;
-		}
-	}
+bool bool_check(string input) {
+	if (input.length() == 1 && input[0] == '0' || input[0] == '1') return true;
+	else return false;
 }
 
-bool repair() {
-	while (true)
-	{
-		bufer();
-		bool rep;
-		cin >> rep;
-		if (!cin)
-		{
-			cout << "Pipe under repair (0; 1): ";
-		}
-		else
-		{
-			return rep;
-		}
-	}
-}
-
-int workshops() {
-	while (true)
-	{
-		bufer();
-		int wshop;
-		cin >> wshop;
-		if (!cin)
-		{
-			cout << "Enter the number of the workshops: ";
-		}
-		else
-		{
-			return wshop;
-		}
-	}
-}
-
-int workshops_in_operation(int wshop) {
-	while (true)
-	{
-		bufer();
-		int wshop_in_op;
-		cin >> wshop_in_op;
-		if (!cin || wshop_in_op > wshop)
-		{
-			cout << "Workshops in operation: ";
-		}
-		else
-		{
-			return wshop_in_op;
-		}
-	}
-}
-
-double effectiveness() {
-	while (true)
-	{
-		bufer();
-		double eff;
-		cin >> eff;
-		if (!cin)
-		{
-			cout << "Effectiveness: ";
-		}
-		else
-		{
-			return eff;
-		}
-	}
-}
-
-string split(string str)
-{
-	for (int i = 0; i < str.length() ; i++)
-	{
-		if (str[i] == ':')
-		{
+string split(string str) {
+	for (int i = 0; i < str.length() ; i++) {
+		if (str[i] == ':') {
 			str.erase(0, i + 2);
 			return str;
 		}
 	}
 }
 
-Pipe add_pipe() {
-	Pipe p;
-	cout << "Enter the name of the pipe: "; cin >> p.name;
-	cout << "Enter the length of the pipe: "; p.length = get_length();
-	cout << "Enter the diameter of the pipe: "; p.diameter = get_diameter();
-	cout << "Pipe under repair (0; 1): "; p.repair = repair();
+Pipe add_pipe(Pipe& p) {
+	string len, diam, rep;
+	while (true) {
+		cout << "Enter the name of the pipe: "; getline(cin, p.name);
+		if (p.name.empty()) cout << "Error!" << endl;
+		else break;
+	}
+	while (true) {
+		cout << "Enter the length of the pipe: "; getline(cin, len);
+		if (int_check(len) == true) {
+			p.length = stoi(len);
+			if (p.length > 0) break;
+			else cout << "Error!" << endl;
+		}
+		else cout << "Error!" << endl;
+	}
+	while (true) {
+		cout << "Enter the diameter of the pipe: "; getline(cin, diam);
+		if (int_check(diam) == true) {
+			p.diameter = stoi(diam);
+			if (p.diameter > 0) break;
+			else cout << "Error!" << endl;
+			break;
+		}
+		else cout << "Error!" << endl;
+	}
+	while (true) {
+		cout << "Pipe under repair (0; 1): "; getline(cin, rep);
+		if (bool_check(rep) == true) {
+			p.repair = stoi(rep);
+			break;
+		}
+		else cout << "Error!" << endl;
+	}
 	return p;
 }
 
-Station add_station() {
-	Station s;
-	cout << "Enter the name of the compressor station: "; cin >> s.name;
-	cout << "Enter the number of the workshops: "; s.workshops = workshops();
-	cout << "Workshops in operation: "; s.workshops_in_operation = workshops_in_operation(s.workshops);
-	cout << "Effectiveness: "; s.effectiveness = effectiveness();
+Station add_station(Station& s) {
+	string wsh, wshio, eff;
+	while (true) {
+		cout << "Enter the name of the compressor station: "; getline(cin, s.name);
+		if (s.name.empty()) cout << "Error!" << endl;
+		else break;
+	}
+	while (true) {
+		cout << "Enter the number of the workshops: "; getline(cin, wsh);
+		if (int_check(wsh) == true) {
+			s.workshops = stoi(wsh);
+			if (s.workshops > 0) break;
+			else cout << "Error!" << endl;
+		}
+		else cout << "Error!" << endl;
+	}
+	while (true) {
+		cout << "Workshops in operation: "; getline(cin, wshio);
+		if (int_check(wshio) == true) {
+			s.workshops_in_operation = stoi(wshio);
+			if (s.workshops >= s.workshops_in_operation) break;
+			else cout << "Error!" << endl;
+		}
+		else cout << "Error!" << endl;
+	}
+	while (true) {
+		cout << "Effectiveness: "; getline(cin, eff);
+		if (int_check(eff) == true) {
+			s.effectiveness = stoi(eff);
+			break;
+		}
+		else cout << "Error!" << endl;
+	}
 	return s;
 }
 
 void edit_pipe(Pipe& p) {
-	if (p.name.empty())
-	{
-		cout << "Please add a pipe." << endl;
-	}
-	else
-	{
-		cout << "Pipe under repair (0; 1): "; p.repair = repair();
+	if (p.name.empty()) cout << "Please add a pipe." << endl;
+	else {
+		while (true) {
+			string rep;
+			cout << "Pipe under repair (0; 1): "; getline(cin, rep);
+			if (bool_check(rep) == true) {
+				p.repair = stoi(rep);
+				break;
+			}
+			else cout << "Error!" << endl;
+		}
 	}
 }
 
 void edit_station(Station& s) {
-	if (s.name.empty())
-	{
-		cout << "Please add a compressor station." << endl;
-	}
-	else
-	{
-		cout << "Workshops in operation: "; s.workshops_in_operation = workshops_in_operation(s.workshops);
+	if (s.name.empty()) cout << "Please add a compressor station." << endl;
+	else {
+		while (true) {
+			string wshio;
+			cout << "Workshops in operation: "; getline(cin, wshio);
+			if (int_check(wshio) == true) {
+				s.workshops_in_operation = stoi(wshio);
+				if (s.workshops >= s.workshops_in_operation) break;
+				else cout << "Error!" << endl;
+			}
+			else cout << "Error!" << endl;
+		}
 	}
 }
 
-void data(Pipe p, Station s) {
-	if (p.name.empty())
-	{
-		cout << "Please add a pipe." << endl;
-	}
-	else
-	{
+void data(const Pipe& p, const Station& s) {
+	if (p.name.empty()) cout << "Please add a pipe." << endl;
+	else {
 		cout << "Pipe name: " << p.name
 			<< "\nPipe length: " << p.length
 			<< "\nPipe diameter: " << p.diameter
 			<< "\nRepair: " << p.repair << endl;
 	}
-	if (s.name.empty())
-	{
-		cout << "Please add a compressor station." << endl;
-	}
-	else
-	{
+	if (s.name.empty()) cout << "Please add a compressor station." << endl;
+	else {
 		cout << "Station name: " << s.name
 			<< "\nWorkshops: " << s.workshops
 			<< "\nWorkshops in operation: " << s.workshops_in_operation
@@ -204,25 +162,48 @@ void data(Pipe p, Station s) {
 	}
 }
 
-void to_file(Pipe p, Station s) {
-	if (p.name.empty() || s.name.empty())
-	{
-		cout << "Please add a pipe and a station." << endl;
-	}
-	else
-	{
+void to_file(const Pipe& p, const Station& s) {
+	if (p.name.empty() && s.name.empty()) cout << "Please add a pipe and a station." << endl;
+	else if (!p.name.empty() && s.name.empty()) {
 		ofstream file;
 		file.open("data.txt", ios::out);
-		if (file.is_open())
-		{
-			file << "Pipe name: " << p.name
-				<< "\nPipe length: " << p.length
-				<< "\nPipe diameter: " << p.diameter
-				<< "\nRepair: " << p.repair
-				<< "\nStation name: " << s.name
-				<< "\nWorkshops: " << s.workshops
-				<< "\nWorkshops in operation: " << s.workshops_in_operation
-				<< "\nEffectiveness: " << s.effectiveness << endl;
+		if (file.is_open()) {
+			file << "Pipe:" << endl
+				<< p.name << endl
+				<< p.length << endl
+				<< p.diameter << endl
+				<< p.repair << endl;
+			file.close();
+			cout << "Saving completed!" << endl;
+		}
+	}
+	else if (p.name.empty() && !s.name.empty()) {
+		ofstream file;
+		file.open("data.txt", ios::out);
+		if (file.is_open()) {
+			file << "Station:" << endl
+				<< s.name << endl
+				<< s.workshops << endl
+				<< s.workshops_in_operation << endl
+				<< s.effectiveness << endl;
+			file.close();
+			cout << "Saving completed!" << endl;
+		}
+	}
+	else {
+		ofstream file;
+		file.open("data.txt", ios::out);
+		if (file.is_open()) {
+			file << "Pipe:" << endl
+				<< p.name << endl
+				<< p.length << endl
+				<< p.diameter << endl
+				<< p.repair << endl << endl
+				<< "Station:" << endl
+				<< s.name << endl
+				<< s.workshops << endl
+				<< s.workshops_in_operation << endl
+				<< s.effectiveness << endl;
 			file.close();
 			cout << "Saving completed!" << endl;
 		}
@@ -233,57 +214,65 @@ void from_file(Pipe& p, Station& s) {
 	ifstream file;
 	string line;
 	file.open("data.txt", ios::in);
-	if (file.is_open())
-	{
-		while (getline(file, line))
-		{
-			if (line.find("Pipe name: ") != string::npos)
-			{
-				p.name = split(line);
+	if (file.is_open()) {
+		while (getline(file, line)) {
+			if (line.find("Pipe:") != string::npos) {
+				if (!p.name.empty()) {
+					while (true) {
+						string overwrite;
+						cout << "Overwrite pipe data? (yes or no)" << endl; getline(cin, overwrite);
+						if (overwrite == "yes") {
+							getline(file, line); p.name = line;
+							getline(file, line); p.length = stoi(line);
+							getline(file, line); p.diameter = stoi(line);
+							getline(file, line); p.repair = stoi(line);
+							break;
+						}
+						else if (overwrite == "no") break;
+					}
+				}
+				else {
+					getline(file, line); p.name = line;
+					getline(file, line); p.length = stoi(line);
+					getline(file, line); p.diameter = stoi(line);
+					getline(file, line); p.repair = stoi(line);
+				}
 			}
-			else if (line.find("Pipe length: ") != string::npos)
-			{
-				p.length = stod(split(line));
-			}
-			else if (line.find("Pipe diameter: ") != string::npos)
-			{
-				p.diameter = stod(split(line));
-			}
-			else if (line.find("Repair: ") != string::npos)
-			{
-				p.repair = stoi(split(line));
-			}
-			else if (line.find("Station name: ") != string::npos)
-			{
-				s.name = split(line);
-			}
-			else if (line.find("Workshops: ") != string::npos)
-			{
-				s.workshops = stoi(split(line));
-			}
-			else if (line.find("Workshops in operation: ") != string::npos)
-			{
-				s.workshops_in_operation = stoi(split(line));
-			}
-			else if (line.find("Effectiveness: ") != string::npos)
-			{
-				s.effectiveness = stod(split(line));
+			else if (line.find("Station:") != string::npos) {
+				if (!s.name.empty()) {
+					while (true) {
+						string overwrite;
+						cout << "Overwrite station data? (yes or no)" << endl; getline(cin, overwrite);
+						if (overwrite == "yes") {
+							getline(file, line); s.name = line;
+							getline(file, line); s.workshops = stoi(line);
+							getline(file, line); s.workshops_in_operation = stoi(line);
+							getline(file, line); s.effectiveness = stoi(line);
+							break;
+						}
+						else if (overwrite == "no") break;
+					}
+				}
+				else {
+					getline(file, line); s.name = line;
+					getline(file, line); s.workshops = stoi(line);
+					getline(file, line); s.workshops_in_operation = stoi(line);
+					getline(file, line); s.effectiveness = stoi(line);
+				}
 			}
 		}
 		file.close();
 		cout << "Loading completed!" << endl;
 	}
-	else
-	{
-		cout << "Error! File not found!" << endl;
-	}
+	else cout << "Error! File not found!" << endl;
 }
 
 void menu() {
 	Pipe pipe;
 	Station station;
-	while (true)
-	{
+	string input;
+	int num;
+	while (true) {
 		cout << "MENU" << endl
 			<< "1. Add a pipe" << endl
 			<< "2. Add a compressor station" << endl
@@ -294,21 +283,16 @@ void menu() {
 			<< "7. Download" << endl
 			<< "0. Exit" << endl
 			<< "Please enter an integer from 0 to 7: ";
-		int num;
-		cin >> num;
-		if (!cin)
-		{
-			cout << "Error! Enter an integer from 0 to 7." << endl;
-		}
-		else
-		{
+		getline(cin, input);
+		if (int_check(input) == true) {
+			num = stoi(input);
 			switch (num)
 			{
 			case 1:
-				pipe = add_pipe();
+				pipe = add_pipe(pipe);
 				break;
 			case 2:
-				station = add_station();
+				station = add_station(station);
 				break;
 			case 3:
 				data(pipe, station);
@@ -332,7 +316,7 @@ void menu() {
 				cout << "Error! Enter an integer from 0 to 7." << endl;
 			}
 		}
-		bufer();
+		else cout << "Error! Enter an integer from 0 to 7." << endl;
 	}
 }
 
